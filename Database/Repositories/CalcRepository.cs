71880dbfd89fd7827d108f49de40b8f877bf292a
unknown <32268498+DimitarMilushev@users.noreply.github.com>
2024-11-19 15:54:19 +0200

    init CalcRepository with GetFactions and GetFactionCreatures methods

---
+++ Database/Repositories/CalcRepository.cs
@@ -0,0 +1,20 @@
+﻿using Database.Entities;
+
+namespace Database.Repositories
+{
+    internal class CalcRepository(CreatureInfoContext dbContext)
+    {
+        private readonly CreatureInfoContext dbContext = dbContext;
+
+        public ICollection<Faction> GetFactions()
+        {
+            return [.. dbContext.Factions];
+        }
+
+        public ICollection<Creature> GetFactionCreatures(int factionID)
+        {
+            // TODO: Maybe handle if the faction doesn't exist.
+            return [.. dbContext.Creatures.Where(c => c.FactionID == factionID)];
+        }
+    }
+}
