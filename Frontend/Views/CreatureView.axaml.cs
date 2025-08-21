f4657701055c2ebabcb39bc531659b86b973651e
Tragun <decho.dechev96@gmail.com>
2025-08-21 18:09:38 +0300

    Adding views and viewmodel files

---
+++ Frontend/Views/CreatureView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Frontend.Views;
+
+public partial class CreatureView : UserControl
+{
+    public CreatureView()
+    {
+        InitializeComponent();
+    }
+}
