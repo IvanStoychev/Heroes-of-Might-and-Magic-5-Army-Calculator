8701f2b53214452a46980f860e4c2f75cb107ae8
ddechew <decho.dechev96@gmail.com>
2025-09-29 17:55:01 +0300

    Initial creation of faction view branch

---
+++ Frontend/ViewModels/FactionViewModel.cs
@@ -0,0 +1,4 @@
+﻿namespace Frontend.ViewModels;
+public class FactionViewModel : ViewModelBase
+{
+}
---
+++ Frontend/Views/FactionView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Frontend;
+
+public partial class FactionView : UserControl
+{
+    public FactionView()
+    {
+        InitializeComponent();
+    }
+}
